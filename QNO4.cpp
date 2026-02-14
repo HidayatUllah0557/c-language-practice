a5a993ed5972330b5187f50f4c67664e61c2a9a1
HidayatUllah0557 <p250557@pwr.nu.edu.pk>
2026-02-13 23:15:10 -0800

    Add files via upload

---
+++ QNO4.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int num,first,second,third;
+	printf("Enter the number\n");
+	scanf("%d",&num);
+	first=(num/100);
+	second=(num%100)/10;
+	third=(num%100)%10;
+	if(first==third){
+		printf("the word is palindrome\n");
+	}else {
+		printf("the word is not plindrome");
+	}
+	return 0;
+}
